f0b3570239145aef0c74b96c5d53d82c6bb59324
JulioCM <julyomanzano@gmail.com>
2022-08-24 20:23:36 -0300

    Tipos de datos- funciones- Operaciones

--- src/com/example/HolaMundo.java
+++ src/com/example/HolaMundo.java
@@ -14,14 +14,51 @@ public class HolaMundo {
         De
         Comentarios
          */
+
+
+        //Funcion void
         ImprimirHolaMundo();
+        //Funcion Retorno String
+        System.out.println(returnHolaMundo());
         TiposDeDatos();
+        //Operaciones - If
+        Sumar(2,3);
+        Restar(5,3);
+        Dividir(8,2);
+        Multiplicar(2,4);
+    }
+
+    private static void Dividir(double a, double b) throws NumberFormatException{
+        try{
+            if(b == 0){
+                throw new NumberFormatException();
+            }
+            System.out.println(a/b);
+        }
+        catch(Error e){
+            throw new NumberFormatException();
+        }
+    }
+
+    private static void Restar(double a, double b) {
+        System.out.println(a-b);
+    }
+
+    private static void Sumar(double a, double b) {
+        System.out.println(a+b);
+    }
+
+    private static void Multiplicar(double a, double b) {
+        System.out.println(a*b);
+
     }
 
     private static void ImprimirHolaMundo() {
         System.out.println("Hola Mundo");
     }
-
+    private static String returnHolaMundo(){
+        return "Hola Mundo";
+    }
     private static void TiposDeDatos() {
         byte number1 = 1;
         short number2 = 2;
